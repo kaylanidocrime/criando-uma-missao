56f1759c5dce5bdee8ce5a203e3e10112ca6b258
kaylani <106694090+kaylanidocrime@users.noreply.github.com>
2024-06-05 14:36:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-const  caixaPricipal = document.querySelector(
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Já agendou sua sobrancelha?",
+        alternativas: [
+            "Sim!",
+            "Ainda não!"
+        ]
+    },
+    {
+        enunciado: "Qual método vc mais gosta?",
+        alternativas: [
+            "Pinça.",
+            "Cera.",
+        ]
+    },
+    {
+        enunciado: "Quem também ama a sensação pós fazer a sobrancelha?",
+        alternativas: [
+            "Eu amoo!!",
+            "Prefiro taturana!"
+        ]
+    },
+    {
+        enunciado: "Como você prefere a henna?",
+        alternativas: [
+            "Natural.",
+            "Marcada."
+        ]
+    },
+    {
+        enunciado: "Qual a melhor opção para você?",
+        alternativas: [
+           "Henna.",
+            "Micropigmentação."
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
