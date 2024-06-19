50a771a4e0abda11131c2d6534e75c9757ea2492
kaylani <106694090+kaylanidocrime@users.noreply.github.com>
2024-06-19 15:46:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Obrigada por responder :)";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
