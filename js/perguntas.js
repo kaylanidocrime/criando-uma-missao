4b7ecc6a6bbf84fb09dea379d6a99c319ef6161f
kaylani <106694090+kaylanidocrime@users.noreply.github.com>
2024-08-14 14:43:21 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,2 +1,67 @@
 export const perguntas = [
   {
+    enunciado: "Já marcou sua sobrancelha?",
+        alternativas: [
+            {
+                texto: "Sim!",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Ainda não!",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Qual modelo de sobrancelha vc acha melhor?",
+        alternativas: [
+            {
+                texto: "Arqueada.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Curvada.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Qual método vc prefere?",
+        alternativas: [
+            {
+                texto: "Pinça.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Cera.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Qual procedimento vc acha melhor?",
+        alternativas: [
+            {
+                texto: "Henna.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "micropigmentação.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    {
+        enunciado: "Como vc prefere a henna? ",
+        alternativas: [
+            {
+                texto: "Natural.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Marcada.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+];
